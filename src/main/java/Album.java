4b78c6741235b7d5c0dea9e2d98490780df84f72
Blake Womack and Grace Stuart <grace@>
2017-03-21 15:35:10 -0700

    add method to fail third test

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,16 +1,20 @@
 public class Album {
   private String mTitle;
-  // private String mArtist;
+  private String mArtist;
   // private String mGenre;
 
-  public Album(String title) {
+  public Album(String title, String artist) {
     mTitle = title;
-    //mArtist = artist;
+    mArtist = artist;
     //mGenre = genre;
   }
 
   public String getTitle() {
     return mTitle;
 
+  }
+
+  public String getArtist() {
+    return "";
   }
 }
