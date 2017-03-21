6ad9ebd12f52208f58e419508807213e2e1ba865
Blake Womack and Grace Stuart <blake@>
2017-03-21 12:45:29 -0700

    add failing test

--- src/test/java/CDTest.java
+++ src/test/java/CDTest.java
@@ -6,7 +6,7 @@ public class CDTest {
   @Test
   public void Cd_createsObjectCorrectly_true() {
     Cd newCd = new Cd("Dark Side of The Moon");
-    assertEquals(true, newCd instanceof Cd);
-  }
+    assertEquals(true, newCd instanceof Cd());
+  } // to pass this test we need to create a CD class, and the Cd method which creates an instance of
 
 }
